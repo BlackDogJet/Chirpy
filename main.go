411b3a991bb1fcabfaf5aaa3f7eedf152b466742
blackdogjet@protonmail.com <blackdogjet@protonmail.com>
2026-01-27 17:27:14 -0600

    1: Created server

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	// Use the http.NewServeMux() function to create an empty servemux.
+	mux := http.NewServeMux()
+
+	corsMux := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		if r.Method == http.MethodOptions {
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+			return
+		}
+		mux.ServeHTTP(w, r)
+	})
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: corsMux,
+	}
+
+	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+}
